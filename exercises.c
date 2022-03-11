54d82daaf5a3c8d486c4567aad9610997e435d13
Your Name <you@example.com>
2022-03-11 16:27:18 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -55,7 +55,7 @@ typedef struct {
 
 
 Persona* crearPersona(char nombre[], char rut[], int edad) {
-   Persona* sujeto =(Persona*) malloc (sinzeof(Persona) );
+   Persona* sujeto =(Persona*) malloc (sizeof(Persona) );
    strcpy(sujeto -> nombre, nombre );
    strcpy(sujeto -> rut , rut );
    sujeto -> edad = edad; 
@@ -103,6 +103,10 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
+    int largo = a -> capacidad;
+    for(int i = 0 ; i < largo ; i++ ){
+       c -> datos[i] = a -> datos[i] = b -> datos[i] ;
+    }
 
 }
 
@@ -112,5 +116,13 @@ Use las operaciones implementadas de vectores para
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
+Vector* a = crearVector( 2 );
+asignarValor(a,0,a1);
+asignarValor(a,1,a2);
+
+Vector* b = crearVector( 2 );
+asignarValor(b,0,b1);
+asignarValor(b,1,b2);
+sumaV(a, b, c );
 
 }
